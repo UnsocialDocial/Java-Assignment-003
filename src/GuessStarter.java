cd94901e0accbf3854673303f459ca2fe58dfc5e
Brody <brodyballiett@gmail.com>
2024-02-04 22:21:58 -0800

    Assignment 3

--- src/GuessStarter.java
+++ src/GuessStarter.java
@@ -1,5 +1,5 @@
 import java.util.Random;
-
+import java.util.Scanner;
 /**
  * Starter code for the "Guess My Number" exercise.
  */
@@ -9,7 +9,13 @@ public class GuessStarter {
         // pick a random number
         Random random = new Random();
         int number = random.nextInt(100) + 1;
-        System.out.println(number);
+        Scanner in = new Scanner(System.in);
+        System.out.print("I'm thinking of a number between 1 and 100 (including both). Can you guess what it is?: ");
+        int guess = in.nextInt();
+        System.out.println("Your guess: "+ guess);
+        System.out.print("The number I was thinking of is: " + number);
+        System.out.print("\nThe difference is: " + Math.abs(number - guess));
     }
 
 }
+
